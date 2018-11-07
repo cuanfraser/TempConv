ff702837ffc2b6e082e567c488c6ea89aa07c354
Cuan <33520989+cuanfraser@users.noreply.github.com>
2018-11-06 19:46:07 -0500

    Allows both C and F conversion, ignore all object files

--- tempconv.c
+++ tempconv.c
@@ -1,22 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+float
+ftoc(float inf) {
+	float temp = inf - 32;
+	float out = temp / 1.8;    
+	return out;
+}
+
+float
+ctof(float inc) {
+	float temp = inc * 1.8;
+	float out = temp + 32;
+	return out;
+}
 
 int
 main(int argc, char* argv[]) {
 	
-	if (argc != 2) {
-		printf("Usage: Input single integer number\n");
+	float out;
+
+	if (argc != 3) {	
+		printf("Usage: tempconv given_temp_unit given_temp_value\n");
+		return 0;
+	}
+    
+	char unit = argv[1][0];
+	float in = atof(argv[2]);
+
+	if (unit == 'C' || unit == 'c') {
+		out = ctof(in);
+	}
+
+	else if (unit == 'F' || unit == 'f') {
+		out = ftoc(in);
+	}
+
+	else {
+		printf("Usage: tempconv given_temp_unit given_temp_value\n");
 		return 0;
 	}
 
-	int in = atol(argv[1]);
-
-	int temp = in - 32;
-	
-	float out = temp / 1.8;
-
 	printf("Output: %f\n", out);
-
 	return 0;
 
 }
